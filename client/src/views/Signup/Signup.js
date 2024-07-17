e18999766dc409935e5be697c526e143a9981f30
sauravshete2003 <sauravshete72@gmail.com>
2024-07-17 17:34:02 +0530

    changes the signup page design

--- client/src/views/Signup/Signup.js
+++ client/src/views/Signup/Signup.js
@@ -3,7 +3,7 @@ import toast, { Toaster } from "react-hot-toast";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import "./Signup.css";
-import logo from './sign-up.avif'
+import BackgroundImg from "./signup-img.jpg";
 
 function Signup() {
   const [user, SetUser] = useState({
@@ -38,65 +38,55 @@ function Signup() {
 
   return (
     <div className="auth-container">
-      <div className="form-container">
-        <h1 className="auth-heading">User Registration</h1>
-        <form className="auth-form">
-          <div className="form-group">
-            <input
-              type="text"
-              placeholder="Enter Name"
-              className="input-box"
-              value={user.fullName}
-              onChange={(e) => {
-                SetUser({ ...user, fullName: e.target.value });
-              }}
-            />
-            <input
-              type="text"
-              placeholder="Enter Email"
-              className="input-box"
-              value={user.email}
-              onChange={(e) => {
-                SetUser({ ...user, email: e.target.value });
-              }}
-            />
-            <input
-              type="password"
-              placeholder="Enter Password"
-              className="input-box"
-              value={user.password}
-              onChange={(e) => {
-                SetUser({ ...user, password: e.target.value });
-              }}
-            />
-            <input
-              type="text"
-              placeholder="Address"
-              className="input-box"
-              value={user.address}
-              onChange={(e) => {
-                SetUser({ ...user, address: e.target.value });
-              }}
-            />
-            <button
-              type="button"
-              onClick={signup}
-              className="auth-btn"
-            >
-              Register
-            </button>
-            <Link to="/login" className="auth-link">Already have an account? Login</Link>
+      <img src={BackgroundImg} className="auth-img" alt="Signup Illustration" />
+      <h1 className="signup-heading">User Registration</h1>
+      <form className="">
+        <div className="auth-input-container">
+          <input
+            type="text"
+            placeholder="Enter Name"
+            className="input-box"
+            value={user.fullName}
+            onChange={(e) => {
+              SetUser({ ...user, fullName: e.target.value });
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Enter Email"
+            className="input-box"
+            value={user.email}
+            onChange={(e) => {
+              SetUser({ ...user, email: e.target.value });
+            }}
+          />
+          <input
+            type="password"
+            placeholder="Enter Password"
+            className="input-box"
+            value={user.password}
+            onChange={(e) => {
+              SetUser({ ...user, password: e.target.value });
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Address"
+            className="input-box"
+            value={user.address}
+            onChange={(e) => {
+              SetUser({ ...user, address: e.target.value });
+            }}
+          />
+          <button type="button" onClick={signup} className="auth-btn">
+            Register
+          </button>
+          <Link to="/login" className="auth-link">
+            Already have an account? Login
+          </Link>
+        </div>
+      </form>
 
-          </div>
-        </form>
-      </div>
-      <div className="auth-img-container">
-        <img
-          src={logo}
-          className="auth-img"
-          alt="Signup Illustration"
-        />
-      </div>
       <Toaster />
     </div>
   );
